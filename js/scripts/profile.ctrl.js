9df17fbde37552a6802c0768a8524f4079d46cca
navneet <hello_navneet@hotmail.com>
2017-06-24 23:08:16 +0530

    gets user by github username, displays on page, alerts about image dimension.

--- js/scripts/profile.ctrl.js
+++ js/scripts/profile.ctrl.js
@@ -7,18 +7,46 @@
     var vm = this;
     vm.search = search;
 
+    vm.user = {
+      "login": "techraiders",
+      "id": 19530178,
+      "avatar_url": "https://avatars1.githubusercontent.com/u/19530178?v=3",
+      "gravatar_id": "",
+      "url": "https://api.github.com/users/techraiders",
+      "html_url": "https://github.com/techraiders",
+      "followers_url": "https://api.github.com/users/techraiders/followers",
+      "following_url": "https://api.github.com/users/techraiders/following{/other_user}",
+      "gists_url": "https://api.github.com/users/techraiders/gists{/gist_id}",
+      "starred_url": "https://api.github.com/users/techraiders/starred{/owner}{/repo}",
+      "subscriptions_url": "https://api.github.com/users/techraiders/subscriptions",
+      "organizations_url": "https://api.github.com/users/techraiders/orgs",
+      "repos_url": "https://api.github.com/users/techraiders/repos",
+      "events_url": "https://api.github.com/users/techraiders/events{/privacy}",
+      "received_events_url": "https://api.github.com/users/techraiders/received_events",
+      "type": "User",
+      "site_admin": false,
+      "name": "Navneet Prakash",
+      "company": "CityExploro",
+      "blog": "https://in.linkedin.com/in/techraiders",
+      "location": "New Delhi, India",
+      "email": null,
+      "hireable": true,
+      "bio": null,
+      "public_repos": 17,
+      "public_gists": 16,
+      "followers": 0,
+      "following": 4,
+      "created_at": "2016-05-23T10:20:16Z",
+      "updated_at": "2017-04-03T19:47:20Z"
+    };
+
     var el = document.getElementById('imageId');
     el.addEventListener('change', utilityService.checkPhoto, false);
 
     function search(userName) {
-      $http.get('https://api.github.com/users/' + userName).then(function(response) {
-        console.log(response.data);
-        vm.user = response.data;
-      }, function(reason) {
-        console.log(reason);
+      searchService.search(userName).then(function(response) {
+        vm.user = response;
       });
     }
-
-
   }
 })();
--- js/scripts/search.service.js
+++ js/scripts/search.service.js
@@ -6,6 +6,17 @@
   function Service($http) {
     var service = {};
 
+    service.search = function(username) {
+      return $http.get('https://api.github.com/users/' + username)
+        .then(function(response) {
+          console.log(response.data);
+          return response.data;
+        }, function(reason) {
+          console.log(reason);
+          return reason.data;
+        });
+    };
+
     return service;
   }
 })();
