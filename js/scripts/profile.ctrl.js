da0a4402df0f44f72e149f28ea3f426f26cf4ce8
navneet <hello_navneet@hotmail.com>
2017-06-24 21:54:48 +0530

    Searches specified username on github.

--- js/scripts/profile.ctrl.js
+++ js/scripts/profile.ctrl.js
@@ -1,13 +1,23 @@
 (function() {
   'use strict';
   angular.module('githubApp')
-    .controller('ProfileSearch', ['searchService', 'utilityService', Controller]);
+    .controller('ProfileSearch', ['searchService', 'utilityService', '$http', Controller]);
 
-  function Controller(searchService, utilityService) {
+  function Controller(searchService, utilityService, $http) {
     var vm = this;
+    vm.search = search;
 
     var el = document.getElementById('imageId');
     el.addEventListener('change', utilityService.checkPhoto, false);
+
+    function search(userName) {
+      $http.get('https://api.github.com/users/' + userName).then(function(response) {
+        console.log(response.data);
+        vm.user = response.data;
+      }, function(reason) {
+        console.log(reason);
+      });
+    }
 
 
   }
