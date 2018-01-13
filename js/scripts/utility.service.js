dfc40aa6d42232bd365d64c865225ac3f7b39bd9
Navneet Prakash <hello_navneet@hotmail.com>
2018-01-13 23:26:47 +0530

    Additional technique gets image real height, width

--- js/scripts/utility.service.js
+++ js/scripts/utility.service.js
@@ -52,3 +52,18 @@
     };
   }
 })();
+
+
+/*
+One massive help provided by CSS in aiding developers to create responsive designs is max-width.  I always set img, iframe, and embed tags to max-width: 100% so that they aren't allowed bleed through their parent and stretch the page on mobiles, but I recently asked myself if it was possible to get an image's "true" size with JavaScript.  I wanted the real image dimensions without any CSS modifications.  MDN informed me of a property I'd never heard of  which provided me the information I wanted!
+
+The JavaScript
+Using some JavaScript magic, we can get the real dimensions of an image:
+
+myImage.addEventListener('load', function() {
+	console.log('My width is: ', this.naturalWidth);
+	console.log('My height is: ', this.naturalHeight);
+});
+
+I had never heard of these read only naturalWidth and naturalHeight properties but I'm happy they're available.  No matter how terrible CSS mangles an img element, the original image dimensions are available.  Of course the image's onload event should be triggered before checking the image dimensions as you cannot know them without the image being loaded.
+*/
